18da5503fdc830b18464f2b36c48ef9c2798360b
Edmund Hogan <ehogan1@uchicago.edu>
2023-08-23 18:46:30 -0400

    basic ics and csv files as well as checkers bot directory added

--- Calendar_Schedular/ics_to_csv.py
+++ Calendar_Schedular/ics_to_csv.py
@@ -1,16 +1,24 @@
 import vobject
 import csv
+import sys
 
-with open('sample.csv', mode='w') as csv_out:
+# def isc_to_csv(csv_out, filename):
+csv_out = str(sys.argv[1])
+filename = str(sys.argv[2])
+with open(csv_out, mode='w') as csv_out:
     csv_writer = csv.writer(csv_out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow(['WHAT', 'WHO', 'FROM', 'TO', 'DESCRIPTION'])
 
     # read the data from the file
-    data = open("sample.ics").read()
+    data = open(filename).read()
 
     # iterate through the contents
     for cal in vobject.readComponents(data):
         for component in cal.components():
             if component.name == "VEVENT":
                 # write to csv
-                csv_writer.writerow([component.summary.valueRepr(),component.attendee.valueRepr(),component.dtstart.valueRepr(),component.dtend.valueRepr(),component.description.valueRepr()])
+                csv_writer.writerow([component.summary.valueRepr(), \
+                                    component.attendee.valueRepr(), \
+                                    component.dtstart.valueRepr(), \
+                                    component.dtend.valueRepr(), \
+                                    component.description.valueRepr()])
