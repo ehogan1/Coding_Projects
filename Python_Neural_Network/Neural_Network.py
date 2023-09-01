b3aac9141966807c91a4a8216f1caf1703143a49
Edmund Hogan <ehogan1@uchicago.edu>
2023-09-01 12:03:23 -0400

    testing the csv conversion

--- Python_Neural_Network/Neural_Network.py
+++ Python_Neural_Network/Neural_Network.py
@@ -1,3 +1,7 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+
+training_data = pd.read_csv('C:\Users\Edmund Hogan\Coding_Projects\Python_Neural_Network\mnist_train.csv')
+
+print(training_data.tail(1))
